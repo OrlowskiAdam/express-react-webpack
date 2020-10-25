9a7216e4aca46e127839e027695161969a6f632c
Adam Orłowski <feruupl@gmail.com>
2020-10-25 13:44:30 +0100

    i

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,19 +1,10 @@
 import * as express from 'express';
 import apiRouter from './routes';
-import * as path from "path";
+import * as path from 'path';
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/public')));
-
-
-//production mode
-if (process.env.NODE_ENV === 'production') {
-    app.get('*', (req, res) => {
-        res.sendfile(path.join(__dirname + '/public/index.html'));
-    })
-}
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(apiRouter);
 
 const port = process.env.PORT || 3000;
