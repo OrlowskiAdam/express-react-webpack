c164c07815c27a53d6ab611f18abfd25003d1a17
Adam Orłowski <feruupl@gmail.com>
2020-10-25 13:48:33 +0100

    removed public dir from .gitignore

--- src/server/server.ts
+++ src/server/server.ts
@@ -4,7 +4,7 @@ import * as path from 'path';
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 app.use(apiRouter);
 
 const port = process.env.PORT || 3000;
